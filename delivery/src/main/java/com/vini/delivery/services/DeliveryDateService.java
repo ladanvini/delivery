5e7127d1d8a7305b85c1101d5e98f91561fc3a33
ladanvini <ladan.pourvatan@gmail.com>
2023-07-13 20:38:16 +0200

    feat: get valid DeliveyDates based on range and weekdays

--- delivery/src/main/java/com/vini/delivery/services/DeliveryDateService.java
+++ delivery/src/main/java/com/vini/delivery/services/DeliveryDateService.java
@@ -1,10 +1,13 @@
 package com.vini.delivery.services;
 
 import java.time.DayOfWeek;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.TimeZone;
 
 import com.vini.delivery.entities.DeliveryDate;
 
@@ -16,9 +19,50 @@ public class DeliveryDateService {
         Set<DayOfWeek> validWeekDays)
     {
         List<DeliveryDate> deliveryDates = new ArrayList<>();
+        if(postalCode == null || postalCode == "")
+            throw new IllegalArgumentException("Invalid Postal Code");
+        if(validDateRange == null || validDateRange.length != 2)
+            throw new IllegalArgumentException("Invalid Date Range");
+        if(validWeekDays == null)
+            throw new IllegalArgumentException("Invalid Week Days");
 
+        // If no valid weekdays, then no valid delivery dates
+        if(validWeekDays.size() ==0)
+            return deliveryDates;
+        Calendar start = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.of("UTC")));
+        start.setTime(validDateRange[0]);
+        Calendar end = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.of("UTC")));
+        end.setTime(validDateRange[1]);
+        if(end.get(Calendar.HOUR_OF_DAY)!=0 || end.get(Calendar.MINUTE)!=0 || end.get(Calendar.SECOND)!=0 || end.get(Calendar.MILLISECOND)!=0)
+            throw new IllegalArgumentException("Invalid End of Date range. Must end at Midnight.");
 
+        if(validWeekDays.contains(calendarDayToDayOfWeek(start.get(Calendar.DAY_OF_WEEK))))
+            deliveryDates.add(new DeliveryDate(postalCode, start.getTime(), this.isGreen(start)));
+        start.set(Calendar.HOUR_OF_DAY, 0);
+        start.set(Calendar.MINUTE, 0);
+        start.set(Calendar.SECOND, 0);
+        start.set(Calendar.MILLISECOND, 0);
+        
+        while(start.get(Calendar.DATE) != end.get(Calendar.DATE)) {
+            if(validWeekDays.contains(calendarDayToDayOfWeek(start.get(Calendar.DAY_OF_WEEK))))
+                deliveryDates.add(new DeliveryDate(postalCode, start.getTime(), this.isGreen(start)));
+            start.add(Calendar.DATE, 1);
+        }
+        
         return deliveryDates;
     }
 
+    private boolean isGreen(Calendar date){
+        /*If the date (day of month) is divisible by 7 or 13, it us green delivery!
+        * (So basically 7, 13, 14, 21, 26 of each month)
+        */
+        return (date.get(Calendar.DATE)%7==0) || (date.get(Calendar.DATE)%13==0);
+    }
+
+    private DayOfWeek calendarDayToDayOfWeek(int calendarDay) {
+        int day = (calendarDay + 6)%7;
+        if(day == 0)
+            return DayOfWeek.SUNDAY;
+        return DayOfWeek.of(day);
+    }
 }
--- delivery/src/test/java/com/vini/delivery/services/delivery/GetValidDeliveryDatesTests.java
+++ delivery/src/test/java/com/vini/delivery/services/delivery/GetValidDeliveryDatesTests.java
@@ -25,11 +25,56 @@ public class GetValidDeliveryDatesTests {
     }
 
     @Test
-    public void whenOnlyValidDayIsToday_thenReturnsTodayAtNow(){
+    public void whenOnlyValidDateIsToday_thenReturnsTodayAtNow(){
         Date startDate = Date.from(Instant.parse("2023-07-10T12:12:12.00Z"));
         Date endDate = Date.from(Instant.parse("2023-07-10T00:00:00.00Z"));
         Date[] range = {startDate, endDate};
         List<DeliveryDate> actual=deliveryService.getValidDeliveryDates("123456", range, new HashSet<>(Arrays.asList(DayOfWeek.MONDAY)));
         assertEquals(1, actual.size());
+        assertEquals(startDate, actual.get(0).getDeliveryDate());
+        assertFalse(actual.get(0).getIsGreenDelivery());
+        assertEquals("123456", actual.get(0).getPostalCode());
     }
+
+    @Test
+    public void whenMultipleValidWeekDaysInTwoWeeks_thenReturnsOnlyThoseWithCorrectAttrs(){
+        Date startDate = Date.from(Instant.parse("2023-07-10T12:12:12.00Z"));
+        Date endDate = Date.from(Instant.parse("2023-07-25T00:00:00.00Z"));
+        Date[] range = {startDate, endDate};
+        List<DeliveryDate> actual=deliveryService.getValidDeliveryDates("123456",
+            range,
+            new HashSet<>(Arrays.asList(DayOfWeek.TUESDAY, DayOfWeek.THURSDAY)));
+        assertEquals(4, actual.size());
+        for (DeliveryDate date:actual){
+            assertEquals("123456", date.getPostalCode());
+        }
+        Date expectedDate0 = Date.from(Instant.parse("2023-07-11T00:00:00.00Z"));
+        Date expectedDate1 = Date.from(Instant.parse("2023-07-13T00:00:00.00Z")); // GREEN!
+        Date expectedDate2 = Date.from(Instant.parse("2023-07-18T00:00:00.00Z"));
+        Date expectedDate3 = Date.from(Instant.parse("2023-07-20T00:00:00.00Z"));
+
+        assertEquals(expectedDate0, actual.get(0).getDeliveryDate());
+        assertFalse(actual.get(0).getIsGreenDelivery());
+
+        assertEquals(expectedDate1, actual.get(1).getDeliveryDate());
+        assertTrue(actual.get(1).getIsGreenDelivery());
+
+        assertEquals(expectedDate2, actual.get(2).getDeliveryDate());
+        assertFalse(actual.get(2).getIsGreenDelivery());
+
+        assertEquals(expectedDate3, actual.get(3).getDeliveryDate());
+        assertFalse(actual.get(3).getIsGreenDelivery());
+
+    }
+
+    @Test
+    public void whenNoValidWeekdaysInRange_thenReturnsEmpty(){
+        Date startDate = Date.from(Instant.parse("2023-07-10T12:12:12.00Z"));
+        Date endDate = Date.from(Instant.parse("2023-07-14T00:00:00.00Z"));
+        Date[] range = {startDate, endDate};
+        List<DeliveryDate> actual=deliveryService.getValidDeliveryDates("123456", range, new HashSet<>(Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)));
+        assertEquals(0, actual.size());
+
+    }
+
 }
