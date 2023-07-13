6ad0d966f73fab4fa01b4fe49c9e3247bec41ab5
ladanvini <ladan.pourvatan@gmail.com>
2023-07-13 18:59:57 +0200

    feat: set up final get delivery dates func

--- delivery/src/main/java/com/vini/delivery/services/DeliveryDateService.java
+++ delivery/src/main/java/com/vini/delivery/services/DeliveryDateService.java
@@ -1,7 +1,24 @@
 package com.vini.delivery.services;
 
+import java.time.DayOfWeek;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import com.vini.delivery.entities.DeliveryDate;
+
 public class DeliveryDateService {
 
+    public List<DeliveryDate> getValidDeliveryDates(
+        String postalCode,
+        Date[] validDateRange,
+        Set<DayOfWeek> validWeekDays)
+    {
+        List<DeliveryDate> deliveryDates = new ArrayList<>();
 
+
+        return deliveryDates;
+    }
 
 }
