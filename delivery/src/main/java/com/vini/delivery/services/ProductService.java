962ca8026eca275607a9e6173501462b919621a7
ladanvini <ladan.pourvatan@gmail.com>
2023-07-13 19:48:52 +0200

    fix: date range should be until day AFTER valid at 00:00

--- delivery/src/main/java/com/vini/delivery/services/ProductService.java
+++ delivery/src/main/java/com/vini/delivery/services/ProductService.java
@@ -129,10 +129,10 @@ public class ProductService {
             return dateRange;
         
         // Set end of range
-        calendar.add(Calendar.DATE, endOfRange);
-        calendar.set(Calendar.HOUR_OF_DAY, 23);
-        calendar.set(Calendar.MINUTE, 59);
-        calendar.set(Calendar.SECOND, 59);
+        calendar.add(Calendar.DATE, endOfRange + 1); // Start of NEXT DAY
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
 
         dateRange[1] = calendar.getTime();
